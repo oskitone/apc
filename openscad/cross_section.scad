oskitone/apc

commit 7768ce125ecca748b5d630f53fde15bdf32af369

Try to fix speaker vs wheel cavities
Wheel well walls weren't thick enough for DFM, probably caused by
the speaker cavity.

Hopefully receding the wheel walls into the speaker cavity provides
more room for DFM bridges to take hold.

openscad/enclosure.scad
@@ -287,7 +287,11 @@ module enclosure_top(
             }
         }
 
-        _cavity(SPEAKER_HEIGHT, SPEAKER_DIAMETER);
+        difference() {
+            _cavity(SPEAKER_HEIGHT, SPEAKER_DIAMETER);
+            _pot_walls(inner_wall);
+        }
+
         _cavity(full_height, SPEAKER_DIAMETER - SPEAKER_RIM * 2);
     }
 

commit a26e3541cc35e43fcbb2e6f698d3ba436752afbe

Try to fix speaker vs wheel cavity collision again
Reverts 7768ce125e, which caused the speaker to no longer perfectly fit
into its cavity.

Additionally, instead of having the two things fight, this change tries
to find room somewhere else by reducing the side exposure just a lilbit.

AND adds a cross section to debug

openscad/cross_section.scad
@@ -4,6 +4,7 @@ CROSS_SECTION_WIDTH = "cross_section_width";
 CROSS_SECTION_HEIGHT = "cross_section_length";
 CROSS_SECTION_BRANDING = "cross_section_branding";
 CROSS_SECTION_WHEEL = "cross_section_wheel";
+CROSS_SECTION_TOP_Z = "cross_section_top_z";
 
 module cross_section(cross_section_name) {
     if (cross_section_name == CROSS_SECTION_WIDTH) {
@@ -25,5 +26,20 @@ module cross_section(cross_section_name) {
     } else if (cross_section_name == CROSS_SECTION_WHEEL) {
         width = PCB_X + PCB_POT_POSITIONS[1][0];
         cube([width, ENCLOSURE_LENGTH, ENCLOSURE_HEIGHT + 10]);
+    } else if (cross_section_name == CROSS_SECTION_TOP_Z) {
+        z = 13.2;
+        offset = 5;
+
+        translate([
+            -offset,
+            -offset,
+            ENCLOSURE_HEIGHT - z
+        ]) {
+            cube([
+                ENCLOSURE_WIDTH + offset * 2,
+                ENCLOSURE_LENGTH + offset * 2,
+                z + offset
+            ]);
+        }
     }
 }

openscad/enclosure.scad
@@ -311,11 +311,7 @@ module enclosure_top(
             }
         }
 
-        difference() {
-            _cavity(SPEAKER_HEIGHT, SPEAKER_DIAMETER);
-            _pot_walls(inner_wall);
-        }
-
+        _cavity(SPEAKER_HEIGHT, SPEAKER_DIAMETER);
         _cavity(full_height, SPEAKER_DIAMETER - SPEAKER_RIM * 2);
     }
 

openscad/shared_constants.scad
@@ -15,7 +15,7 @@ ENCLOSURE_WALL = 2.4;
 ENCLOSURE_INNER_WALL = 1.2;
 ENCLOSURE_FLOOR_CEILING = 1.8;
 ENCLOSURE_INTERNAL_GUTTER = 2;
-ENCLOSURE_SIDE_OVEREXPOSURE = 2;
+ENCLOSURE_SIDE_OVEREXPOSURE = 1.4;
 
 ENCLOSURE_GRILL_DEPTH = 6;
 ENCLOSURE_GRILL_GUTTER = 4;